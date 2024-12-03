5964a3948e8f09823b4504db693b2fc4e14af673
Euphoria <poetdim@gmail.com>
2024-12-02 20:44:55 -0500

    done

--- exercises/23-click-events/click-events.js
+++ exercises/23-click-events/click-events.js
@@ -7,14 +7,22 @@
    * Problem 1: Alert Me
    * When the clicks on the button that says "Alert Me!", it should display an alert.
    * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
-   */
-  // Write your answer here
-  /**
+*/
+  const alertBtn = document.querySelector("#alert");
+  alertBtn.addEventListener("click", () => {
+    alert("Hello World!");
+  });
+
+  /*
    * Problem 2: Disable a button that will charge a credit card.
    *
    * To prevent users from charging the credit card more than once:
    * 1. Disable the button when it is clicked.
    * 2. Change the text to say e.g. "Loading ..." once it is clicked.
    */
-  // Write your answer here
+  const loadingBtn = document.querySelector("#credit");
+  loadingBtn.addEventListener("click", () => {
+    loadingBtn.disabled = true;
+    loadingBtn.textContent = "Loading ...";
+  });
 })();
